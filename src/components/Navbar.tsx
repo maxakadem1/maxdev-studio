ec50f41a843f90ed4e0c5a2693502c6fbefdfc47
Maxim Abdulkhalikov <63835313+maxakadem1@users.noreply.github.com>
2023-10-19 01:06:54 -0500

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+  return (
+    <div className='absolute flex items-center justify-between h-24 w-full flex-row bg-black text-white px-12'>
+      <button>MENU</button>
+      <button>MAXDEV</button>
+      <button>CONTACT</button>
+    </div>
+  )
+}
---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,13 @@
+import '../app/glitch.sass'
+
+export default function Homepage() {
+  return (
+    <div className='flex min-h-screen flex-col items-center justify-center p-24 bg-black text-white'>
+      <div className='wrapper'>
+        <h1 className='text-9xl font-semibold glitch'>MAXDEV</h1>
+      </div>
+
+      <div>studio</div>
+    </div>
+  )
+}
