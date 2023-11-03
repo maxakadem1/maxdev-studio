e646a64c50c28dcb8476df261eaae919420eeaf3
Maxim Abdulkhalikov <63835313+maxakadem1@users.noreply.github.com>
2023-11-03 14:13:51 -0500

    small updates

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,11 @@
 export default function Navbar() {
   return (
     <div className='absolute flex items-center justify-between h-24 w-full flex-row bg-black text-white px-12'>
-      <button>MENU</button>
-      <button>MAXDEV</button>
-      <button>CONTACT</button>
+      <button className='hover:text-[#FF002E] tracking-[0.2rem]'>MENU</button>
+      <button className='hover:text-[#FF002E] tracking-[0.2rem]'>MAXDEV</button>
+      <button className='hover:text-[#FF002E] tracking-[0.2rem]'>
+        CONTACT
+      </button>
     </div>
   )
 }
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -3,11 +3,11 @@ import '../app/glitch.sass'
 export default function Homepage() {
   return (
     <div className='flex min-h-screen flex-col items-center justify-center p-24 bg-black text-white'>
-      <div className='wrapper'>
+      <div className='wrapper '>
         <h1 className='text-9xl font-semibold glitch'>MAXDEV</h1>
       </div>
 
-      <div>studio</div>
+      <div className='text-2xl text-[#FF002E] tracking-[0.5rem]'>STUDIO</div>
     </div>
   )
 }
