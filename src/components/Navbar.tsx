cc0cb56295c8274a89bd7566efdf6a180fbe9b80
Maxim Abdulkhalikov <63835313+maxakadem1@users.noreply.github.com>
2023-11-06 13:48:08 -0600

    navbar improvement

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,23 @@
 export default function Navbar() {
   return (
-    <div className='z-[100] absolute flex items-center justify-between h-24 w-full flex-row bg-black text-white px-12'>
-      <button className='hover:text-[#5f5cff] tracking-[0.2rem]'>MENU</button>
-      <button className='hover:text-[#5f5cff] tracking-[0.2rem]'>MAXDEV</button>
-      <button className='hover:text-[#5f5cff] tracking-[0.2rem]'>
-        CONTACT
-      </button>
+    <div className='text-sm z-[100] absolute flex items-center justify-center h-[120px] w-full flex-row bg-black text-white px-12 '>
+      <div className='flex flex-row gap-5 bg-[#5f5cff1c] rounded-2xl p-3'>
+        <button className=' hover:bg-[#3836b4] h-[45px] w-[100px] rounded-xl'>
+          MAXDEV
+        </button>
+        <button className=' hover:bg-[#3836b4] h-[45px] w-[100px] rounded-xl'>
+          About
+        </button>
+        <button className=' hover:bg-[#3836b4] h-[45px] w-[100px] rounded-xl'>
+          Works
+        </button>
+        <button className=' hover:bg-[#3836b4] h-[45px] w-[100px] rounded-xl'>
+          Process
+        </button>
+        <button className=' hover:bg-[#3836b4] h-[45px] w-[100px] rounded-xl'>
+          Contact
+        </button>
+      </div>
     </div>
   )
 }
