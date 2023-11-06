0315ad56f8f83fec4fc6e5103fa589fb0fce3965
Maxim Abdulkhalikov <63835313+maxakadem1@users.noreply.github.com>
2023-11-06 17:40:16 -0600

    smooth transitions added

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,5 +1,4 @@
 'use client'
-
 import React, { useState } from 'react'
 import Navbar from '@/components/Navbar'
 import Homepage from './content/Homepage'
@@ -8,6 +7,7 @@ import Works from './content/Works'
 import { ComponentName } from '@/types'
 import Process from './content/Process'
 import Contact from './content/Contact'
+import { useSpring, animated, config } from '@react-spring/web'
 
 export default function Main() {
   const [activeComponent, setActiveComponent] = useState<ComponentName>('Home') // default component
@@ -15,6 +15,13 @@ export default function Main() {
   const handleMenuClick = (componentName: ComponentName) => {
     setActiveComponent(componentName)
   }
+
+  const fade = useSpring({
+    from: { opacity: 0 },
+    to: { opacity: 1 },
+    reset: true,
+    config: { duration: 500 }, // adjust duration to control speed of transition
+  })
 
   const renderComponent = () => {
     switch (activeComponent) {
@@ -36,7 +43,7 @@ export default function Main() {
   return (
     <>
       <Navbar onMenuClick={handleMenuClick} />
-      {renderComponent()}
+      <animated.div style={fade}>{renderComponent()}</animated.div>
     </>
   )
 }
