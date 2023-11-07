9760f62c09200bf6a565335666dca33a90d693d9
Maxim Abdulkhalikov <63835313+maxakadem1@users.noreply.github.com>
2023-11-06 18:33:33 -0600

    text changes

--- src/pages/content/Homepage.tsx
+++ src/pages/content/Homepage.tsx
@@ -28,11 +28,16 @@ export default function Homepage() {
       >
         <source src='/MAXDEV.mp4' type='video/mp4' />
       </video>
-
-      <div className='wrapper absolute top-[78%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
+      {/* <div className='wrapper absolute top-[78%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
         <h1 className='text-5xl font-light glitch'>studio</h1>
+      </div> */}
+      <div className='absolute top-[78%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
+        <h1 className='text-5xl font-light glitch'>WebDev / UI / UX</h1>
       </div>
 
+      <div className='absolute top-[82%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
+        <h1 className='text-5xl font-light glitch'>studio</h1>
+      </div>
       {/* <div className='text-2xl text-[#FF002E] tracking-[0.5rem]'>STUDIO</div> */}
     </div>
   )
