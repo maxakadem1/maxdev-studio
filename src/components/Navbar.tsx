3640ba4ca874da399ee6cbbd18b5c7fe80f4e85f
Maxim Abdulkhalikov <63835313+maxakadem1@users.noreply.github.com>
2023-11-09 16:36:46 -0600

    work on services page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,5 @@
 import { ComponentName } from '@/types'
-import { useState, useEffect } from 'react'
-import { useSpring, animated } from 'react-spring'
+
 import Image from 'next/image'
 
 type NavbarProps = {
@@ -8,74 +7,53 @@ type NavbarProps = {
 }
 
 export default function Navbar({ onMenuClick }: NavbarProps) {
-  const [show, setShow] = useState(true)
-  const [lastYPos, setLastYPos] = useState(0)
-
-  const navbarAnimation = useSpring({
-    transform: show ? 'translateY(0)' : 'translateY(-100%)',
-    config: { tension: 350, friction: 30 },
-  })
-
-  useEffect(() => {
-    function handleScroll() {
-      console.log('Scrolling...') // For debugging
-      const yPos = window.scrollY
-      const isScrollingUp = yPos < lastYPos
-
-      setShow(isScrollingUp)
-      setLastYPos(yPos)
-    }
-
-    window.addEventListener('scroll', handleScroll, false)
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll, false)
-    }
-  }, [lastYPos])
-
   return (
-    <animated.div
-      style={navbarAnimation}
-      className='lg:text-sm text-[10px] z-[100] absolute flex flex-row items-center justify-between top-[3%] w-full text-white px-8'
-    >
-      <button
-        onClick={() => onMenuClick('Home')}
-        className='flex flex-row gap-2 items-center justify-center'
-      >
-        <Image src='/MAXDEV.svg' alt='Logo' width={50} height={50} />
-        <div className='flex flex-col items-start justify-start'>
-          <div className='text-md font-bold'>MAXDEV</div>
-          <div className='text-[12px] leading-3 text-gray-500'>web studio</div>
+    <div className='lg:text-sm text-[10px] z-[10] absolute flex flex-row items-center justify-between top-[3%] w-full text-white px-12'>
+      <div className='flex flex-row justify-between items-center w-full backdrop-blur-3xl px-5 py-2 rounded-3xl'>
+        <button
+          onClick={() => onMenuClick('Home')}
+          className='flex flex-row gap-2 items-center justify-center'
+        >
+          <div className='pr-5'>
+            <Image src='/MAXDEV.svg' alt='Logo' width={50} height={50} />
+          </div>
+
+          <div className='flex-col items-start justify-start lg:flex hidden'>
+            <div className='text-md font-bold'>MAXDEV</div>
+            <div className='text-[12px] leading-3 text-gray-500'>
+              web studio
+            </div>
+          </div>
+        </button>
+
+        <div className='flex flex-row gap-5 font-light'>
+          <button
+            onClick={() => onMenuClick('About')}
+            className=' flex items-center justify-center transition duration-300 ease-in-out hover:text-[#5f5cff]'
+          >
+            Services
+          </button>
+          <button
+            onClick={() => onMenuClick('Works')}
+            className=' flex items-center justify-center  transition duration-300 ease-in-out hover:text-[#5f5cff]'
+          >
+            Works
+          </button>
+
+          <button
+            onClick={() => onMenuClick('Process')}
+            className=' flex items-center justify-center  transition duration-300 ease-in-out hover:text-[#5f5cff]'
+          >
+            Process
+          </button>
+          <button
+            onClick={() => onMenuClick('Contact')}
+            className=' flex items-center justify-center transition duration-300 ease-in-out hover:text-[#5f5cff]'
+          >
+            Contact
+          </button>
         </div>
-      </button>
-
-      <div className='flex flex-row gap-5 font-light'>
-        <button
-          onClick={() => onMenuClick('About')}
-          className='flex items-center justify-center transition duration-300 ease-in-out hover:text-[#5f5cff]'
-        >
-          Services
-        </button>
-        <button
-          onClick={() => onMenuClick('Works')}
-          className='flex items-center justify-center  transition duration-300 ease-in-out hover:text-[#5f5cff]'
-        >
-          Works
-        </button>
-
-        <button
-          onClick={() => onMenuClick('Process')}
-          className='flex items-center justify-center  transition duration-300 ease-in-out hover:text-[#5f5cff]'
-        >
-          Process
-        </button>
-        <button
-          onClick={() => onMenuClick('Contact')}
-          className='flex items-center justify-center  transition duration-300 ease-in-out hover:text-[#5f5cff]'
-        >
-          Contact
-        </button>
       </div>
-    </animated.div>
+    </div>
   )
 }
--- src/pages/content/Services.tsx
+++ src/pages/content/Services.tsx
@@ -1,3 +1,7 @@
+import { FiArrowUpRight } from 'react-icons/fi'
+import { BiLogoLinkedin } from 'react-icons/bi'
+import { AiFillInstagram } from 'react-icons/ai'
+
 export default function Services() {
   return (
     <div className='z-[1] flex pt-32 w-screen flex-col items-center justify-center bg-black text-white'>
@@ -45,38 +49,118 @@ export default function Services() {
       </div>
       {/* MAIN */}
       <div className='w-full px-20'>
-        <div className='border rounded-lg p-20 flex flex-row w-full'>
-          <div className='flex flex-col gap-8 w-[50%]'>
-            <div className='lg:text-5xl text-xl font-bold'>WEB DEVELOPMENT</div>
-            <div>
-              Being present online is not enough anymore. You must leave your
-              mark if you want to be seen and heard. And that’s where MAXDEV
-              comes in. We are experienced and capable of creating a simple
-              website or a complex immersive web platform for your needs.
-              Technology evolves, but so do we. Ready to take the game to the
-              next level?
+        <div className='border border-gray-700 rounded-lg w-full'>
+          {/* ELEMENT 1 */}
+          <div className='flex flex-row p-20'>
+            <div className='flex flex-col gap-8 w-[50%] items-start'>
+              <div className='lg:text-5xl text-xl font-bold'>
+                WEB DEVELOPMENT
+              </div>
+              <div>
+                Being present online is not enough anymore. You must leave your
+                mark if you want to be seen and heard. And that’s where MAXDEV
+                comes in. We are experienced and capable of creating a simple
+                website or a complex immersive web platform for your needs.
+                Technology evolves, but so do we. Ready to take the game to the
+                next level?
+              </div>
+              <button>learn more</button>
+            </div>
+            <div className='w-[50%] flex items-center justify-center'>
+              image here
             </div>
           </div>
-          <div className='w-[50%] flex items-center justify-center'>
-            image here
+
+          <div className='mx-20 '>
+            <hr className=' border-gray-700' />
           </div>
-        </div>
-        <div className='border rounded-lg p-20 flex flex-row w-full'>
-          <div className='flex flex-col gap-8 w-[50%]'>
-            <div className='lg:text-5xl text-xl font-bold'>WEB DEVELOPMENT</div>
-            <div>
-              Being present online is not enough anymore. You must leave your
-              mark if you want to be seen and heard. And that’s where MAXDEV
-              comes in. We are experienced and capable of creating a simple
-              website or a complex immersive web platform for your needs.
-              Technology evolves, but so do we. Ready to take the game to the
-              next level?
+
+          {/* ELEMENT 2 */}
+          <div className='flex flex-row p-20'>
+            <div className='flex flex-col gap-8 w-[50%] items-start'>
+              <div className='lg:text-5xl text-xl font-bold'>
+                WEB DEVELOPMENT
+              </div>
+              <div>
+                Being present online is not enough anymore. You must leave your
+                mark if you want to be seen and heard. And that’s where MAXDEV
+                comes in. We are experienced and capable of creating a simple
+                website or a complex immersive web platform for your needs.
+                Technology evolves, but so do we. Ready to take the game to the
+                next level?
+              </div>
+              <button>learn more</button>
+            </div>
+            <div className='w-[50%] flex items-center justify-center'>
+              image here
             </div>
           </div>
-          <div className='w-[50%] flex items-center justify-center'>
-            image here
+
+          <div className='mx-20 '>
+            <hr className=' border-gray-700' />
+          </div>
+
+          {/* ELEMENT 3 */}
+          <div className='flex flex-row p-20'>
+            <div className='flex flex-col gap-8 w-[50%] items-start'>
+              <div className='lg:text-5xl text-xl font-bold'>
+                WEB DEVELOPMENT
+              </div>
+              <div>
+                Being present online is not enough anymore. You must leave your
+                mark if you want to be seen and heard. And that’s where MAXDEV
+                comes in. We are experienced and capable of creating a simple
+                website or a complex immersive web platform for your needs.
+                Technology evolves, but so do we. Ready to take the game to the
+                next level?
+              </div>
+              <button>learn more</button>
+            </div>
+            <div className='w-[50%] flex items-center justify-center'>
+              image here
+            </div>
           </div>
         </div>
+      </div>
+
+      {/* FOOTER */}
+      <div className='h-screen flex items-center justify-center px-20'>
+        <div className='flex flex-row w-full'>
+          <div className='w-[50%]'>
+            <div className='text-7xl font-semibold'>
+              LET'S CREATE TOMORROW TODAY.
+            </div>
+          </div>
+          <div className='w-[50%] z-10 flex flex-col gap-5 text-sm p-8 justify-start items-start'>
+            <div className='flex flex-col gap-3 items-start justify-start w-full'>
+              <button className='w-full bg-[#5f5cff73] hover:bg-[#5f5cffb6] flex flex-row items-center justify-between gap-2 p-3 rounded-xl transition duration-300 ease-in-out'>
+                <span>START A PROJECT</span>
+                <FiArrowUpRight className='text-xl' />
+              </button>
+              <button className='w-full bg-[#5f5cff73] hover:bg-[#5f5cffb6] flex flex-row items-center justify-between gap-2 p-3 rounded-xl transition duration-300 ease-in-out'>
+                <span>CONTACT US</span>
+                <FiArrowUpRight className='text-xl' />
+              </button>
+              <button className='w-full bg-[#5f5cff73] hover:bg-[#5f5cffb6] flex flex-row items-center justify-between gap-2 p-3 rounded-xl transition duration-300 ease-in-out'>
+                <span>SCHEDULE A MEETING</span>
+                <FiArrowUpRight className='text-xl' />
+              </button>
+            </div>
+
+            <div className='flex flex-row gap-2 justify-start'>
+              <button>
+                <BiLogoLinkedin className='text-2xl' />
+              </button>
+              <button>
+                <AiFillInstagram className='text-2xl' />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className='w-full text-start px-20 text-[10px] py-4'>
+        © MAXDEV studio 2023. All Rights Reserved
       </div>
     </div>
   )
