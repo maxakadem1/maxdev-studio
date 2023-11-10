5913674064c6304ddaa4103e8e6582f6c606be47
Maxim Abdulkhalikov <63835313+maxakadem1@users.noreply.github.com>
2023-11-10 16:09:22 -0600

    updates to webdev page

--- src/pages/content/Services.tsx
+++ src/pages/content/Services.tsx
@@ -1,9 +1,9 @@
 import { FiArrowUpRight } from 'react-icons/fi'
-import { BiLogoLinkedin } from 'react-icons/bi'
-import { AiFillInstagram } from 'react-icons/ai'
+
 import { useEffect, useRef } from 'react'
 import Link from 'next/link'
 import { ComponentName } from '@/types'
+import Footer from '@/components/Footer'
 
 type ServicesProps = {
   onMenuClick: (componentName: ComponentName) => void
@@ -158,44 +158,7 @@ export default function Services({ onMenuClick }: ServicesProps) {
       </div>
 
       {/* FOOTER */}
-      <div className='h-screen flex items-center justify-center px-20'>
-        <div className='flex flex-row w-full'>
-          <div className='w-[50%]'>
-            <div className='text-7xl font-semibold'>
-              LET'S CREATE TOMORROW TODAY.
-            </div>
-          </div>
-          <div className='w-[50%] z-10 flex flex-col gap-5 text-sm p-8 justify-start items-start'>
-            <div className='flex flex-col gap-3 items-start justify-start w-full'>
-              <button className='w-full bg-[#5f5cff73] hover:bg-[#5f5cffb6] flex flex-row items-center justify-between gap-2 p-3 rounded-xl transition duration-300 ease-in-out'>
-                <span>START A PROJECT</span>
-                <FiArrowUpRight className='text-xl' />
-              </button>
-              <button className='w-full bg-[#5f5cff73] hover:bg-[#5f5cffb6] flex flex-row items-center justify-between gap-2 p-3 rounded-xl transition duration-300 ease-in-out'>
-                <span>CONTACT US</span>
-                <FiArrowUpRight className='text-xl' />
-              </button>
-              <button className='w-full bg-[#5f5cff73] hover:bg-[#5f5cffb6] flex flex-row items-center justify-between gap-2 p-3 rounded-xl transition duration-300 ease-in-out'>
-                <span>SCHEDULE A MEETING</span>
-                <FiArrowUpRight className='text-xl' />
-              </button>
-            </div>
-
-            <div className='flex flex-row gap-2 justify-start'>
-              <button>
-                <BiLogoLinkedin className='text-2xl' />
-              </button>
-              <button>
-                <AiFillInstagram className='text-2xl' />
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className='w-full text-start px-20 text-[10px] py-4'>
-        © MAXDEV studio 2023. All Rights Reserved
-      </div>
+      <Footer />
     </div>
   )
 }
--- src/pages/services/Webdev.tsx
+++ src/pages/services/Webdev.tsx
@@ -1,8 +1,138 @@
+import { useEffect, useRef } from 'react'
+import { BiLogoReact } from 'react-icons/bi'
+import { BiLogoJavascript } from 'react-icons/bi'
+import { BiLogoTypescript } from 'react-icons/bi'
+import { FaNodeJs } from 'react-icons/fa'
+
 export default function Webdev() {
+  // FOR VIDEO
+  const videoRef = useRef<HTMLVideoElement>(null)
+
+  useEffect(() => {
+    // Check if the videoRef is currently being pointed to an element
+    if (videoRef.current) {
+      // Set the playback rate here. 0.5 is half the normal speed.
+      videoRef.current.playbackRate = 1
+    }
+  }, [])
+
   return (
     <>
-      <div className='z-[1] flex pt-32 w-screen flex-col items-center justify-center bg-black text-white'>
-        <div>Webdev page</div>
+      <div className='z-[1] flex pt-40 w-screen flex-col items-center justify-center bg-black text-white'>
+        {/* INTRO */}
+        <div className='px-12 flex flex-col items-center gap-5 w-full'>
+          <div className='text-6xl font-bold'>WEB DEVELOPMENT</div>
+          <div className='w-[50%]'>
+            At MAXDEV studio, web development is at our core. We meticulously
+            research, organize data, design, and develop to exceed our clients'
+            expectations. Our expertise ranges from immersive web development to
+            WordPress, and custom solutions, ready to impress even the most
+            discerning clients.
+          </div>
+          <video
+            className='w-[50%]'
+            ref={videoRef}
+            autoPlay
+            loop
+            muted
+            playsInline
+          >
+            <source src='/GlassBlocksHiRes.mp4' type='video/mp4' />
+          </video>
+        </div>
+        {/* INFO */}
+        <div className='flex flex-col items-center gap-5 w-full px-40'>
+          <div className='flex flex-row gap-20 w-full items-center'>
+            <div className='w-[50%] text-3xl font-semibold'>
+              <div className='text-sm font-normal text-gray-700'>01</div>
+              Immersive Web Experience
+            </div>
+            <div className='w-[50%]'>
+              You have just one chance for a first impression, and you deserve
+              the best one. With our skills and know-how, your company or
+              product will be presented on a whole new interactive level. Share
+              your story through an immersive web presentation and impress your
+              website visitors with amazing visualizations, animations, 3D
+              design, and a remarkable web experience.
+            </div>
+          </div>
+
+          <div className='m-20 w-full'>
+            <hr className=' border-gray-700' />
+          </div>
+
+          <div className='flex flex-row gap-20 w-full items-center'>
+            <div className='w-[50%] text-3xl font-semibold'>
+              <div className='text-sm font-normal text-gray-700'>02</div>
+              WordPress Development
+            </div>
+            <div className='w-[50%]'>
+              You have just one chance for a first impression, and you deserve
+              the best one. With our skills and know-how, your company or
+              product will be presented on a whole new interactive level. Share
+              your story through an immersive web presentation and impress your
+              website visitors with amazing visualizations, animations, 3D
+              design, and a remarkable web experience.
+            </div>
+          </div>
+
+          <div className='m-20 w-full'>
+            <hr className=' border-gray-700' />
+          </div>
+
+          <div className='flex flex-row gap-20 w-full items-center'>
+            <div className='w-[50%] text-3xl font-semibold'>
+              <div className='text-sm font-normal text-gray-700'>03</div>
+              Custom Web Development
+            </div>
+            <div className='w-[50%]'>
+              You have just one chance for a first impression, and you deserve
+              the best one. With our skills and know-how, your company or
+              product will be presented on a whole new interactive level. Share
+              your story through an immersive web presentation and impress your
+              website visitors with amazing visualizations, animations, 3D
+              design, and a remarkable web experience.
+            </div>
+          </div>
+
+          <div className='m-20 w-full'>
+            <hr className=' border-gray-700' />
+          </div>
+
+          <div className='flex flex-row gap-20 w-full items-center'>
+            <div className='w-[50%] text-3xl font-semibold'>
+              <div className='text-sm font-normal text-gray-700'>04</div>
+              Product Development
+            </div>
+            <div className='w-[50%]'>
+              You have just one chance for a first impression, and you deserve
+              the best one. With our skills and know-how, your company or
+              product will be presented on a whole new interactive level. Share
+              your story through an immersive web presentation and impress your
+              website visitors with amazing visualizations, animations, 3D
+              design, and a remarkable web experience.
+            </div>
+          </div>
+
+          <div className='m-20 w-full'>
+            <hr className=' border-gray-700' />
+          </div>
+        </div>
+        {/* tech stack */}
+        <div className='flex flex-row justify-between items-center w-full px-40 py-10'>
+          <div className='text-sm'>Technologies</div>
+          <div className='flex flex-row gap-10 text-7xl'>
+            <BiLogoReact />
+            <BiLogoJavascript />
+            <BiLogoTypescript />
+            <FaNodeJs />
+          </div>
+        </div>
+        {/* process */}
+        <div className='px-40 py-10 w-full flex flex-row gap-5'>
+          <div>text</div>
+          <div>text</div>
+        </div>
       </div>
     </>
   )
