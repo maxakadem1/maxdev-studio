b1d387bc8a9f719cbefb4cb996305023cb2e6832
Maxim Abdulkhalikov <63835313+maxakadem1@users.noreply.github.com>
2023-11-28 00:11:52 -0600

    works mobile changes

--- src/pages/content/Works.tsx
+++ src/pages/content/Works.tsx
@@ -1,10 +1,11 @@
 import Footer from '@/components/Footer'
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { FiArrowUpRight } from 'react-icons/fi'
 import { FaArrowRight } from 'react-icons/fa'
 import Link from 'next/link'
 
 export default function Works() {
+  const [windowWidth, setWindowWidth] = useState<number>(window.innerWidth)
   const videoRef = useRef<HTMLVideoElement>(null)
 
   useEffect(() => {
@@ -13,12 +14,19 @@ export default function Works() {
       // Set the playback rate here. 0.5 is half the normal speed.
       videoRef.current.playbackRate = 0.6
     }
+
+    const handleResize = () => setWindowWidth(window.innerWidth)
+    window.addEventListener('resize', handleResize)
+
+    return () => window.removeEventListener('resize', handleResize)
   }, [])
 
   return (
     <div className='z-[1] flex pt-32 w-screen flex-col items-center justify-center bg-black text-white'>
-      <div className='w-[50%] flex items-start justify-center flex-col gap-6'>
-        <div className='text-6xl font-bold'>FEATURED WORK</div>
+      <div className='lg:w-[50%] w-full flex items-start justify-center flex-col gap-6 px-8'>
+        <div className='lg:text-6xl text-3xl font-bold text-center'>
+          FEATURED WORK
+        </div>
         <div>
           With a proven track record spanning diverse industries, we excel in
           serving clients across the world. Our expertise transcends boundaries,
@@ -30,7 +38,7 @@ export default function Works() {
       {/* CARDs */}
       <div className='pt-16 w-full px-20 flex justify-center'>
         {/* card 1 */}
-        <div className=' p-5 rounded-lg flex flex-col justify-between items-center w-[25%] h-[450px] relative'>
+        <div className=' p-5 rounded-lg flex flex-col justify-between items-center lg:w-[25%] w-full h-[450px] relative'>
           {/* Pseudo-element for background image with reduced opacity */}
           <div
             className='absolute inset-0 rounded-xl'
@@ -44,7 +52,7 @@ export default function Works() {
 
           {/* Ensuring content is above the background image */}
           <div className='relative z-10 text-[11px]'>Current Client</div>
-          <div className='relative z-10 text-2xl font-bold'>
+          <div className='relative z-10 text-2xl font-bold text-center'>
             OUTREACH GENIUS
           </div>
           <div className='relative z-10 text-[11px] text-center'>
@@ -59,24 +67,49 @@ export default function Works() {
           <FiArrowUpRight className='text-xl' />
         </button>
       </div>
-      <div className='w-full px-20'>
-        <span className='w-full flex items-center justify-center text-6xl font-bold'>
+      <div className='w-full lg:px-20 px-4'>
+        <span className='w-full flex items-center justify-center lg:text-6xl text-3xl font-bold text-center'>
           OTHER PROJECTS
         </span>
         {/* project 1 */}
-        <Link
-          href='https://outreachgenius.ai/'
-          target='_blank'
-          className='flex flex-row w-full justify-between my-20 text-gray-400 hover:text-white cursor-pointer'
-        >
-          <div>01</div>
-          <div>Outreach Genius</div>
-          <div>Custom Web Development, UI/UX Design</div>
-          <div>2023</div>
-          <div>
-            <FaArrowRight />
-          </div>
-        </Link>
+        {windowWidth > 1028 ? (
+          <Link
+            href='https://outreachgenius.ai/'
+            target='_blank'
+            className='flex flex-row lg:text-base text-sm w-full justify-between my-20 text-gray-400 hover:text-white cursor-pointer'
+          >
+            <div>01</div>
+            <div>Outreach Genius</div>
+            <div>Custom Web Development, UI/UX Design</div>
+            <div>2023</div>
+            <div>
+              <FaArrowRight />
+            </div>
+          </Link>
+        ) : (
+          <>
+            <Link
+              href='https://outreachgenius.ai/'
+              target='_blank'
+              className='flex flex-col w-full gap-1  justify-between my-20 text-gray-400 hover:text-white cursor-pointer'
+            >
+              <div className='flex flex-row justify-between items-center '>
+                <div className='flex flex-row gap-5'>
+                  <div>01</div>
+                  <div>Outreach Genius</div>
+                </div>
+                <div className='flex flex-row  gap-3 justify-center items-center h-full'>
+                  <div className=''>2023</div>
+                  <FaArrowRight className='flex justify-center items-center h-full' />
+                </div>
+              </div>
+
+              <div className='text-[12px]'>
+                Custom Web Development, UI/UX Design
+              </div>
+            </Link>
+          </>
+        )}
       </div>
       {/* footer */}
       <Footer />
